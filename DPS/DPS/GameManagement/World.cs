6964879f5cb17790435f717688d63c5d50ac9cb7
DESKTOP-QGL8FB8\dautz <s.p.dautzenberg@students.uu.nlcd>
2016-11-18 17:45:05 +0100

    Plz work lolz

--- DPS/DPS/GameManagement/World.cs
+++ DPS/DPS/GameManagement/World.cs
@@ -1,12 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DPS.GameManagement
+namespace DPS
 {
-    class World
+    class World : ObjectList
     {
+        public World(string id) : base(id)
+        {
+
+        }
+
+        public virtual void Load(string assetName)
+        {
+
+            
+        }
+
+        private List<string> ReadFile(string assetName)
+        {
+            StreamReader stream = new StreamReader(assetName);
+            List<string> lines = new List<string>();
+
+
+            //read lines from file
+            string line = stream.ReadLine();
+            while (line != null)
+            {
+                lines.Add(line);
+                line = stream.ReadLine();
+            }
+            return lines;
+        }
+
+        private void ReadTiles(List<string> lines)
+        {
+            foreach(string tile in lines)
+            {
+                //for(int i = 0; i < tile.Length;)
+            }
+        }
     }
 }
