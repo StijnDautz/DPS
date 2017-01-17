f3d159ea0548612a559b4051c4aeb022ee1f0d76
Bart-PC\Bart <b.kolb@live.>
2017-01-17 16:45:13 +0100

    Dungeon levels toegevoegd, plaatjes toegevoegd, dat was het. DOEI

--- DPS/DPS/Content/Classes/Objectgrids/DungeonIce4.cs
+++ DPS/DPS/Content/Classes/Objectgrids/DungeonIce4.cs
@@ -6,13 +6,13 @@ using System.Threading.Tasks;
 using Engine;
 namespace Content
 {
-    class DungeonIce4 : ObjectGrid
+    class Level1 : ObjectGrid
     {
-            public DungeonIce4(string id, string assetName, int tileSize) : base(id, assetName, tileSize)
-            {
+        public Level1(string id, string assetName, int tileSize) : base(id, assetName, tileSize)
+        {
 
-            }
-            
+        }
+
         private TexturedObject SetupCollisionTile(string id, string assetName)
         {
             TexturedObject Tile = new TexturedObject(id, "Textures/Tiles/" + assetName);
@@ -21,37 +21,49 @@ namespace Content
             return Tile;
         }
 
-            protected override Engine.Object FindType(char type)
+        protected override Engine.Object FindType(char type)
+        {
+            switch (type)
             {
-                switch (type)
-                {
-                    case '1': return SetupCollisionTile("walltile", "1.TileSet4");
-                    case '2': return SetupCollisionTile("walltile", "2.TileSet4");
-                    case '3': return SetupCollisionTile("walltile", "3.TileSet4");
-                    case '4': return SetupCollisionTile("walltile", "4.TileSet4");
-                    case '5': return SetupCollisionTile("walltile", "5.TileSet4");
-                    case '6': return SetupCollisionTile("walltile", "6.TileSet4");
-                    case '7': return SetupCollisionTile("walltile", "7.TileSet4");
-                    case '8': return SetupCollisionTile("walltile", "8.TileSet4");
-                    case '9': return SetupCollisionTile("walltile", "9.TileSet4");
-                    case 'a': return SetupCollisionTile("walltile", "9.TileSet4");
-                    case 'b': return SetupCollisionTile("walltile", "9.TileSet4");
-                    case 'c': return SetupCollisionTile("walltile", "9.TileSet4");
-                    case 'f': return SetupCollisionTile("platformtile", "f.TileSet4");
-                    case 'g': return SetupCollisionTile("platformtile", "g.TileSet4");
-                    case 'h': return SetupCollisionTile("platformtile", "h.TileSet4");
-                    case 'j': return SetupCollisionTile("walltile", "j.TileSet4");
-                    case 'k': return SetupCollisionTile("emptytile", "0.Overworld");
-                    case 'l': return SetupCollisionTile("walltile", "l.TileSet4");
-                    case 'm': return SetupCollisionTile("walltile", "m.TileSet4");
-                    case 'n': return SetupCollisionTile("walltile", "n.TileSet4");
-                    case 'o': return SetupCollisionTile("walltile", "o.TileSet4");
-                    case 'p': return SetupCollisionTile("walltile", "p.TileSet4");
+                case '1': return SetupCollisionTile("walltile", "1.TileSet4");
+                case '2': return SetupCollisionTile("walltile", "2.TileSet4");
+                case '3': return SetupCollisionTile("walltile", "3.TileSet4");
+                case '4': return SetupCollisionTile("walltile", "4.TileSet4");
+                case '5': return SetupCollisionTile("walltile", "5.TileSet4");
+                case '6': return SetupCollisionTile("walltile", "6.TileSet4");
+                case '7': return SetupCollisionTile("walltile", "7.TileSet4");
+                case '8': return SetupCollisionTile("walltile", "8.TileSet4");
+                case '9': return SetupCollisionTile("walltile", "9.TileSet4");
+                case 'a': return SetupCollisionTile("walltile", "9.TileSet4");
+                case 'b': return SetupCollisionTile("walltile", "9.TileSet4");
+                case 'c': return SetupCollisionTile("walltile", "9.TileSet4");
+
+                case 'd': return SetupCollisionTile("door", "d.TileSet4");
+                case 'e': return SetupCollisionTile("door", "e.TileSet4");
+
+                case 'f': return SetupCollisionTile("platformtile", "f.TileSet4");
+                case 'g': return SetupCollisionTile("platformtile", "g.TileSet4");
+                case 'h': return SetupCollisionTile("platformtile", "h.TileSet4");
+
+                case 'i': return SetupCollisionTile("ladder", "i.TileSet4");
+
+                case 'j': return SetupCollisionTile("walltile", "j.TileSet4");
+                case 'k': return SetupCollisionTile("emptytile", "9.TileSet4");
+                case 'l': return SetupCollisionTile("walltile", "l.TileSet4");
+                case 'm': return SetupCollisionTile("walltile", "m.TileSet4");
+                case 'n': return SetupCollisionTile("walltile", "n.TileSet4");
+                case 'o': return SetupCollisionTile("walltile", "o.TileSet4");
+                case 'p': return SetupCollisionTile("walltile", "p.TileSet4");
+
+                case 'q': return SetupCollisionTile("spike", "q.TileSet4");
+                case 'r': return SetupCollisionTile("spike", "r.TileSet4");
+                case '0':
+                case '-': return SetupCollisionTile("emptytile", "0.Overworld");
 
                 default: throw new Exception("character of type: " + type + "was not associated with an Object");
-                }
             }
         }
     }
+}
 
 
