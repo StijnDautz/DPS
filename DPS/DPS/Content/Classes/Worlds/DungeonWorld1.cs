133741a89068393ccbe77917eb7facdafa613ca4
Stijn Dautzenberg <dautzenberg.stijn@gmail.com>
2017-01-18 13:41:03 +0100

    dungeon check

---
+++ DPS/DPS/Content/Classes/Worlds/DungeonWorld1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Content
+{
+    class DungeonWorld1 : Engine.World
+    {
+        public DungeonWorld1(int width, int height) : base(width, height)
+        {
+            for(int i = 1; i < 101; i++)
+            {
+                Add(new Level1("level1", this, i.ToString(), 96));
+            }
+        }
+    }
+}
