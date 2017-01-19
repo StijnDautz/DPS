bb31d603f8e74e60608b27ee9edba4adfc7d0dec
Frank <hokkiefrank@hotmail.com>
2017-01-19 13:28:04 +0100

    Soundeffects Work in Progress

--- DPS/DPS/Engine/Objects/Button.cs
+++ DPS/DPS/Engine/Objects/Button.cs
@@ -4,34 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 
 namespace Engine
 {
     abstract class Button : TexturedObject
     {
+        SoundEffect clickSound; 
 
-        public Button(string id, Object parent, string assetName) : base(id, parent, assetName)
+        public Button(string id, Object parent, string assetName, string soundName) : base(id, parent, assetName)
         {
-
+            clickSound = GameInstance.AssetManager.getSoundEffect("Rocket");
+            
         }
 
+        
         public override void Update(GameTime gameTime)
         {
             base.Update(gameTime);
-
+            
             if (GameInstance.InputManager.LeftMouseButtonPressed)
             {
                 Vector2 mousePosition = GameInstance.InputManager.MousePosition;
                 if (mousePosition.X > Position.X && mousePosition.X < Position.X + Width && mousePosition.Y > Position.Y && mousePosition.Y < Position.Y + Width)
                 {
                     ActionWhenPressed();
+                   
                 }
             }       
         }
 
         protected virtual void ActionWhenPressed()
         {
-
+            clickSound.Play();
         }
     }
 }
