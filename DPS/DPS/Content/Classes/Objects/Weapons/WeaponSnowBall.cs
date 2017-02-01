a9ee4c1dea712ccc50b086a77efde7e3415885cf
DESKTOP-QGL8FB8\dautz <s.p.dautzenberg@students.uu.nlcd>
2017-02-01 01:46:35 +0100

    fixed snowman, changed pickup stuff i guess

--- DPS/DPS/Content/Classes/Objects/Weapons/WeaponSnowBall.cs
+++ DPS/DPS/Content/Classes/Objects/Weapons/WeaponSnowBall.cs
@@ -1,18 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Engine;
+﻿using Engine;
 
 namespace Content
 {
-    class WeaponSnowBall : Engine.Weapon
+    class WeaponSnowBall : Weapon
     {
-        public WeaponSnowBall(string id, Engine.Object parent, Engine.SpriteSheet spriteSheet, Engine.Character owner, int damage) : base(id, parent, spriteSheet, owner, damage)
+        public WeaponSnowBall(string id, Object parent, SpriteSheet spriteSheet, Character owner, int damage) : base(id, parent, spriteSheet, owner, damage)
         {
             HasPhysics = true;
+            CanCollide = true;
             Mass = 0.4f;
         }
+
+        public override void OnCollision(Object collider)
+        {
+            base.OnCollision(collider);
+            if(collider != Owner)
+            {
+                World.Remove(this);
+            }
+        }
     }
+
 }
